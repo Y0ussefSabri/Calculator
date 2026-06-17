94615c8ffd53eae08e0d526151b7480b9c9e7f16
Sabri <hireyoussefsabri@gmail.com>
2026-06-17 16:36:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function compute()
+{
+    const selectOperation = document.querySelector('input[name = "operation"]:checked').value;
+    let firstNum = Number(document.getElementById("first_num").value);
+    let secondNum = Number(document.getElementById("second_num").value);
+
+    console.log(selectOperation);
+    if(selectOperation == '+')
+    document.getElementById("result").textContent = firstNum + secondNum;
+    else if(selectOperation == '-')
+    document.getElementById("result").textContent = firstNum - secondNum;
+    else if(selectOperation == '*')
+    document.getElementById("result").textContent = firstNum * secondNum;
+    else if(selectOperation == '/')
+    document.getElementById("result").textContent = firstNum / secondNum;
+    
+}
