2f7995ce72479f2547112931a8626a18b69cf03b
Sabri <hireyoussefsabri@gmail.com>
2026-06-19 03:06:21 +0300

    third commit

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ function compute()
     document.getElementById("result").textContent = firstNum * secondNum;
     else if(selectOperation == '/')
     document.getElementById("result").textContent = firstNum / secondNum;
-       
+    
 
+}
+
+async function generateAndCompute()
+{
+    // const response = await fetch(
+    //     "https://www.randomnumberapi.com/api/v1.0/random?min=1&max=100&count=2"
+    // );
+
+    // const numbers = await response.json();
+    // console.log(numbers);
+    // document.getElementById("first_num").value = numbers[0];
+    // document.getElementById("second_num").value = numbers[1];
+    fetch("https://www.randomnumberapi.com/api/v1.0/random?min=1&max=100&count=2")
+
+    .then(res => res.json())
+
+    .then(data => console.log(data))
+
+    .catch(err => console.error(err));
 }
